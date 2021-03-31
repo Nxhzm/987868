53280c050c6291f0484e4e08d28145b7ace7ca53
Nahum Solomon <76616055+Nxhzm@users.noreply.github.com>
2021-03-31 20:02:33 +0100

    Add files via upload

---
+++ Question.js
@@ -0,0 +1,18 @@
+class Question{
+
+    Hide(){}
+
+    Display(){
+    this.title.html("MyQuiz Game");
+    this.title.position(350,0);
+
+    this.question.html("Question - What phone is better - Samsung or iPhone?")
+    this.question.Position(150,80);
+    this.option1.html("1: Samsung ");
+    this.option1.position(150, 100);
+    this.option2.html("2:Iphone");
+    this.option2.position(150, 120)
+
+    this.input.position(150, 230);
+    }
+}
